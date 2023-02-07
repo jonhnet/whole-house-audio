c1713a61c81ac2882347fc1e8ba1cc11ba3b4310
Jon Howell <jonh@jonh.net>
2023-02-07 10:00:09 -0800

    draft of git install

--- bluetooth-sink/pi-setup.sh
+++ bluetooth-sink/pi-setup.sh
@@ -14,11 +14,10 @@ sudo bash ./pi-setup-root.sh
 # Configure pulseaudio to forward to RTP.
 #*IMPORTANT**: I added a destination_ip here, aiming the RTP hose at my
 #audio server.
-```
+mkdir -p /home/pi/.config/pulse/
 cat << __EOF__ > /home/pi/.config/pulse/default.pa
 .include /etc/pulse/default.pa
 load-module module-null-sink sink_name=rtp
 load-module module-rtp-send source=rtp.monitor destination_ip=${RTP_LISTENER_IP} port=1760
 set-default-sink rtp
 __EOF__
-```
